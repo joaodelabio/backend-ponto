6843d6f2a226a173743f0adc3c939e5d2577af2a
joaodelabio <joaogabriel.delabio@gmail.com>
2024-11-04 10:39:54 -0300

    backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// app.METODO('rota/caminho', (req, res) => {})
+
+app.get('/users', (req, res) => {
+    res.send("nessa rota vou cagar nos usuarios. -passaro");
+});
+
+app.post('/users', (req, res) => {
+    res.send("cagando nos outros via post. -passaro");
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(`o passaro cagou em ${req.params.id}`);
+});
+
+app.post('/user/:id1-:id2', (req, res) => {
+    res.send(req.params);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`o passaro cagou na porta ${PORT}`);
+});
